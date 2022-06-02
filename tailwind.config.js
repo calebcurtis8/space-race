926ac31cc36739141ceb85fff3f34fd36ef630ba
Caleb Curtis <caleb@softlimit.com>
2022-06-02 18:00:11 -0400

    apply sizes and spacing to hero type, font on button is too heavy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   content: ["./**/*.{liquid,js}"],
   theme: {
     fontFamily: {
-      'univers': ['Univers', 'sans-serif'],
+      'univers-extended': ['Univers Extended', 'sans-serif'],
+      'univers-condensed': ['Univers Condensed', 'sans-serif'],
       'ogg': ['Ogg', 'serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        offwhite: '#F9F8F5',
+        darkgray: '#232323',
+      }
+    },
   },
   plugins: [],
 }
